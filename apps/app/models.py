edaddaaebf896a9d8716a72ff51e5d218cc6594c
James Jung <therealjamesjung@gmail.com>
2022-07-02 01:44:35 +0900

    feat: add inital models

---
+++ apps/app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Diary(models.Model):
+    admin = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    users = models.ManyToManyField(get_user_model, related_name='diary')
+    code = models.CharField(max_length=128)
+
+
+class Question(models.Model):
+    user = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
+    content = models.CharField(max_length=256)
+    month = models.IntegerField(null=True, blank=True)
+    day = models.IntegerField(null=True, blank=True)
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=512)
+    year = models.IntegerField(null=True, blank=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    content = models.CharField(max_length=512)z
