1c760746c22333e46aee09504da6482058055d37
James Jung <therealjamesjung@gmail.com>
2022-07-04 01:37:54 +0900

    refactor: split get question object function

--- apps/app/views.py
+++ apps/app/views.py
@@ -7,8 +7,22 @@ from rest_framework.generics import (CreateAPIView,
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError
 
-from .serializers import DiarySerializer, DiaryWriteSerializer, QuestionSerializer
+from .serializers import AnswerSerializer, DiarySerializer, DiaryWriteSerializer, QuestionSerializer
 from .models import Diary, Question, Answer, Comment
+
+
+def get_question_object(date):
+    try:
+        month, day = [int(x) for x in date.strip().split('-')]
+    except AttributeError:
+        raise ParseError('Date is not given')
+    except ValueError:
+        raise ParseError('Date should be in "[Month]-[Date]" format')
+    try:
+        question = Question.objects.get(month=month, day=day)
+    except Question.DoesNotExist:
+        raise ParseError('Question with given date does not exists')
+    return question
 
 
 class DiaryCreateAPIView(CreateAPIView):
@@ -17,12 +31,6 @@ class DiaryCreateAPIView(CreateAPIView):
 
 class QuestionRetrieveAPIView(APIView):
     def get(self, request):
-        try:
-            month, day = request.query_params.get('date').strip().split('-')
-        except AttributeError:
-            raise ParseError('Date is not given.')
-        except ValueError:
-            raise ParseError('Date should be in "Month-Date" format')
-        queryset = Question.objects.filter(month=int(month), day=int(day))
-        serializer = QuestionSerializer(queryset, many=True)
+        question = get_question_object(request.query_params.get('date'))
+        serializer = QuestionSerializer(question)
         return Response(serializer.data)
