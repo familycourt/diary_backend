c081a36bd219dfdcc069be20937af613643a8811
James Jung <therealjamesjung@gmail.com>
2022-07-04 02:47:03 +0900

    feat: add name field on User

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -5,18 +5,19 @@ from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, username, password=None):
+    def create_user(self, username, name, password=None):
         if not username:
             raise ValidationError('Username must be given.')
-
-        user = User(username=username, password=None)
+        if not name:
+            raise ValidationError('Name must be given.')
+        user = User(username=username, name=name, password=None)
         user.set_password(password)
         user.save()
 
         return user
 
-    def create_superuser(self, username, password=None):
-        superuser = self.create_user(username, password)
+    def create_superuser(self, username, name, password=None):
+        superuser = self.create_user(username, name, password)
         superuser.is_staff = True
         superuser.save()
 
@@ -25,11 +26,13 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser):
     username = models.CharField(max_length=32, unique=True)
+    name = models.CharField(max_length=32)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name', ]
 
     def __str__(self):
         return str(self.username)
--- apps/authentication/serializers.py
+++ apps/authentication/serializers.py
@@ -11,6 +11,7 @@ class UserSerializer(Serializer):
     username = CharField(write_only=True, required=True,
                          max_length=32, min_length=8,
                          validators=[UniqueValidator(get_user_model().objects.all())])
+    name = CharField(write_only=True, required=True, max_length=32)
     password = CharField(write_only=True, required=True,
                          validators=[validate_password])
     password2 = CharField(write_only=True, required=True)
@@ -23,7 +24,7 @@ class UserSerializer(Serializer):
 
     def create(self, validated_data):
         user = get_user_model().objects.create(
-            username=validated_data['username'])
+            username=validated_data['username'], name=validated_data['name'])
         user.set_password(validated_data['password'])
         user.save()
 
